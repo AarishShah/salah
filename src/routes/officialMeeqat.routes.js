91e9f4f121c68d720cdb479a284347e924242587
Alpha <syedaarishshah@gmail.com>
2025-07-20 16:31:36 +0530

    applied rate limiting

--- src/routes/officialMeeqat.routes.js
+++ src/routes/officialMeeqat.routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const controller = require("../controllers/officialMeeqat.controller");
 const Auth = require("../middleware/auth.middleware");
 const { upload } = require("../utils/upload");
+const { uploadRateLimiter } = require("../utils/rateLimiter");
 
 // Middleware
 const adminAuth = Auth({ allowedRoles: ['admin'] });
@@ -16,7 +17,7 @@ router
 // --- Admin Routes ---
 router
     .get("/", adminAuth, controller.getAllOfficialMeeqats)
-    .post("/", adminAuth, upload.single('csvFile'), controller.createOfficialMeeqat)
-    .put("/:id", adminAuth, upload.single('csvFile'), controller.updateOfficialMeeqat);
+    .post("/", adminAuth, uploadRateLimiter, upload.single('csvFile'), controller.createOfficialMeeqat)
+    .put("/:id", adminAuth, uploadRateLimiter, upload.single('csvFile'), controller.updateOfficialMeeqat);
 
 module.exports = router;
